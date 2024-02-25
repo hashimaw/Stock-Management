018cd9de02a66d91898a5e080081f1c5530777e8
Hashim Abdrehman <139076207+hashimaw@users.noreply.github.com>
2024-02-25 17:18:54 +0300

    Add files via upload

---
+++ Public/app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const mongodb = require('mongodb');
+
+const uri = "mongodb+srv://hashTheAdmin:HashTheAdmin@inventorymanagement.ekh6lts.mongodb.net/?retryWrites=true&w=majority&appName=InventoryManagement";
+//express app
+const app = express();
+
+//register view engine
+app.set('view engine', 'ejs');
+
+
+//listen for requests 
+app.listen(9000);
+
+//static files
+app.use(express.static('assets'));
+
+app.get('/', (req, res) => {
+    res.render('dashboard');
+});
+
+app.get('/addemployee', (req, res) => {
+    res.render('addemployee');
+});
+
+app.get('/additem', (req, res) => {
+    res.render('additem');
+});
+
+app.get('/employeeaccounts', (req, res) => {
+    res.render('employeeaccounts');
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.get('/transactionhistory', (req, res) => {
+    res.render('transactionhistory');
+});
+
+app.get('/updateproducts', (req, res) => {
+    res.render('updateproducts');
+});
+
+app.use((req, res) => {
+    res.render('404');
+});
