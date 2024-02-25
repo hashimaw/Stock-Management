36152d245105f2f69cd621f0bcc66c5b521bae91
Hashim Abdrehman <139076207+hashimaw@users.noreply.github.com>
2024-02-25 17:17:37 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './public/views/login.html',
+    './Public/assets/login.css',
+    './Public/views/dashboard.html',
+    './Public/assets/dashboardstyle.css',
+    './public/views/addemployee.html',
+    './Public/assets/addemployeestyle.css',
+    './Public/views/additem.html',
+    './Public/assets/additemstyle.css',
+    './public/views/employeeaccounts.html',
+    './Public/assets/employeeaccountsstyle.css',
+    './Public/views/transactionhistory.html',
+    './Public/assets/transactionhistorystyle.css',
+    './public/views/updateproducts.html',
+    './Public/assets/updateproductsstyle.css'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
